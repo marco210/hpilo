073df07f8280a3415c11dc0f1434f3c9c0cca00b
marco210 <trungnghia021097@gmail.com>
2021-08-30 23:21:04 +0700

    test thermal update1

--- config/chassis_variables.go
+++ config/chassis_variables.go
@@ -11,4 +11,19 @@ var (
 			"line_input_voltage_type",
 		},
 		nil)
+
+	C_temperature_status = prometheus.NewDesc(
+		"ilo_temperature_status",
+		"Chassis temperature {0: OK, 1: Warning, 2: Critical}",
+		[]string{
+			"reading_celsius",
+			"memberid",
+			"sensor_number",
+			"status_health",
+			"status_state",
+			"upper_threshold_critical",
+			"upper_threshold_fatal",
+		},
+		nil,
+	)
 )
